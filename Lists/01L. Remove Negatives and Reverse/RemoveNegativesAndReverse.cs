15e21002b5c3a0282c2fe44cc5e503ccc48a8f42
VenciSarov <venci.sarov@gmail.com>
2017-01-28 21:08:02 +0200

    Adding new Tasks and Solutions. Problem 03L needs to be solved.

---
+++ Lists/01L. Remove Negatives and Reverse/RemoveNegativesAndReverse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class RemoveNegativesAndReverse
+{
+    static void Main()
+    {
+        List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        RemoveNegativeNums(nums);
+        
+    }
+
+    static void RemoveNegativeNums(List<int> nums)
+    {
+        for (int i = 0; i < nums.Count; i++)
+        {
+            if (nums[i] < 0)
+            {
+                nums.RemoveAt(i);
+                i--;
+            }
+        }
+
+        if (nums.Count != 0)
+        {
+            nums.Reverse();
+            Console.WriteLine(string.Join(" ", nums));
+        }
+        else
+        {
+            Console.WriteLine("empty");
+        }
+    }
+}
+
---
+++ Lists/02L. Append Lists/AppendLists.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class AppendLists
+{
+    static void Main()
+    {
+        List<string> inputNums = Console.ReadLine().Trim().Split('|').ToList();
+        inputNums.Reverse();
+
+        AppendReversedList(inputNums);
+    }
+
+    static void AppendReversedList(List<string> inputNums)
+    {
+        List<string> result = new List<string>();
+
+
+        for (int i = 0; i < inputNums.Count; i++)
+        {
+            result.AddRange(inputNums[i].Split(' '));
+        }
+
+        for (int i = 0; i < result.Count; i++)
+        {
+            if (result[i] == "")
+            {
+                result.RemoveAt(i);
+                i--;
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
+
