5d13a2dc62d6ef5a98ad375c8b82b3f5b7d08ad7
VenciSarov <venci.sarov@gmail.com>
2017-01-29 19:14:46 +0200

    changes made.

--- Lists/03L. Sum Adjacent Equal Numbers/SumAdjacentEqualNumbers.cs
+++ Lists/03L. Sum Adjacent Equal Numbers/SumAdjacentEqualNumbers.cs
@@ -6,7 +6,35 @@ class SumAdjacentEqualNumbers
 {
     static void Main()
     {
+        string nums = Console.ReadLine();
 
+        List<decimal> numsList = nums.Split(' ').Select(decimal.Parse).ToList();
+
+        SumAndPrintEqualNumbers(numsList);
+
+        Console.WriteLine(string.Join(" ", numsList));
+    }
+
+    static void SumAndPrintEqualNumbers(List<decimal> numsList)
+    {
+        for (int i = 0; i < numsList.Count - 1; i++)
+        {
+            if (numsList[i] == numsList[i + 1])
+            {
+                numsList[i] = numsList[i] + numsList[i + 1];
+                numsList.RemoveAt(i + 1);
+
+                if (i >= 1)
+                {
+                    i -= 2;
+                }
+                else
+                {
+                    i--;
+                }
+
+            }
+        }
     }
 }
 
