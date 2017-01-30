f6ca016f7a5e84c33a3c5d1e9f4d929ea1f7608c
VenciSarov <venci.sarov@gmail.com>
2017-01-30 08:53:06 +0200

    Problem solved. But need correction. Adding method and etc.

--- Lists/07L. Count Numbers/CountNumbers.cs
+++ Lists/07L. Count Numbers/CountNumbers.cs
@@ -9,9 +9,30 @@ class CountNumbers
         List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
 
         numbers.Sort();
-        
-        int numsCounters = 1;
 
+        int numsCounters = 1;
+        int start = 0;
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            for (int r = start + 1; r < numbers.Count; r++)
+            {
+                if (numbers[start] == numbers[r])
+                {
+                    numsCounters++;
+                    i++;
+                }
+                else
+                {
+                    start = r - 1;
+                    break;
+                }
+            }
+
+            Console.WriteLine($"{numbers[start]} -> {numsCounters}");
+            start++;
+            numsCounters = 1;
+        }
     }
 }
 
